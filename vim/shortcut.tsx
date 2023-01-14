788d0a2c8b8939c0b56517174355c43f0c997279
Vimal Menon <vimal.webs@gmail.com>
2023-01-14 17:35:03 +0530

    Update shortcut.tsx

--- vim/shortcut.tsx
+++ vim/shortcut.tsx
@@ -8,3 +8,7 @@ mu = move the line up
 https://vim.rtorr.com/
 https://viblo.asia/p/vims-flavor-KE7bGoZEM5e2
 https://github.com/amix/vimrc
+https://github.com/mileszs/ack.vim
+https://github.com/mg979/vim-visual-multi
+https://github.com/neoclide/coc.nvim
+https://github.com/dense-analysis/ale#faq-statusline
